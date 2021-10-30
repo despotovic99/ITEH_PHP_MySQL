b5de54dc8c23c0d48d4dd74a5278c9cb03c5d53c
despotovic99 <nemanja@Legion-Dragce.localdomain>
2021-10-30 23:06:57 +0200

    dodata update funkcionalnost

---
+++ handler/update.php
@@ -0,0 +1,19 @@
+<?php
+require "../dbBroker.php";
+require "../model/prijava.php";
+
+if(isset($_POST['id']) && isset($_POST['katedra']) && isset($_POST['predmet'])
+    && isset($_POST['sala']) && isset($_POST['datum'])){
+    $obj = new Prijava($_POST['id'],$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum']);
+    $status = $obj->update($_POST['id'], $conn);
+
+    if($status){
+        echo 'Success';
+    }else{
+        echo $status;
+        echo "Failed";
+    }
+}
+
+
+?> 
